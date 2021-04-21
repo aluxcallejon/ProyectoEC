cf7c95cfb158b4c1a6ec31598ed0367a832b192f
Luis <luigicallejonreche@gmail.com>
2021-04-21 11:10:17 +0200

    Conexion AJAX funcional

--- Pycom/primeraweb.py
+++ Pycom/primeraweb.py
@@ -1,11 +1,25 @@
 import network
 import socket
 import machine
+import json
 
 from machine import Pin
 #Fichero HTML a mandar
 html1 = """
 <html>
+<script>
+var myVar = setInterval(loadDoc, 500);
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "datos.txt", true);
+  xhttp.send();
+}
+</script>
 <head>
 <title>Mi primera página web </title>
 </head>
@@ -14,26 +28,34 @@ html1 = """
 <hr>
 <p>Esto tan sencillo es una verdadera página web, aunque le falta el contenido,
 pero todo llegará.</p>
-<h2> FRASE DEL DÍA: %s <hr>
+<p id="demo"></p>
+
+<h2> FRASE DEL DÍA: BUENOS DIAS <hr>
 </body>
 </html>
 """
 input()
 Boton=Pin('P10',Pin.IN)
-frase=input("Mete frase del día:")
 
 #Setup Socket WebServer
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 80))
 s.listen(5)
+i=0;
 while True:
     conn, addr = s.accept()
     print("Got a connection from %s" % str(addr))
     request = conn.recv(1024)
+
     print(request)
-    response = html1 %frase
+    if 'datos.txt' in request:
+        response='{"vel":"%s"}'%i
+    else:
+        response = html1
+
     conn.send(response)
     conn.close()
+    i=i+1
     if Boton() == 0:
         break
 s.close()
